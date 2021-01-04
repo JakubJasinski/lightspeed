eb9b3d4f1df31d216fa9d89f2e01a5d1e4b96e8a
Jakub <jakub.j.dev@gmail.com>
2021-01-04 21:59:53 +0100

    Fix, fix json-format

--- app/hooks/useQualification.ts
+++ app/hooks/useQualification.ts
@@ -3,7 +3,7 @@ import fetcher from "../fetcher";
 
 const useQulifications = (year) => {
   //http://ergast.com/api/f1/2008/5/qualifying
-  const fetchUrl = `https://ergast.com/api/f1/${year}/5/qualifying.html`;
+  const fetchUrl = `https://ergast.com/api/f1/${year}/5/qualifying.json`;
 
   const { data, error } = useSWR(fetchUrl, fetcher);
   return {
