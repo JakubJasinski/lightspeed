d4eec9a3e63dcc01e93ba8bc7670a61b68e4abd7
Jakub <jakub.j.dev@gmail.com>
2021-01-22 23:09:38 +0100

    Add, d3 example

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Head from "next/head";
-import TestFunction from './TestFetch';
+import TestFunction from "./TestFetch";
+import { useRef, useEffect } from "react";
+import * as d3 from "d3";
 
+const Circle = () => {
+  const ref = useRef();
+
+  useEffect(() => {
+    const svgElement = d3.select(ref.current);
+    svgElement.append("circle").attr("cx", 150).attr("cy", 70).attr("r", 50);
+  }, []);
+
+  return <svg ref={ref} />;
+};
 export default function Home() {
-  
-
   return (
     <div>
       <Head>
@@ -12,7 +22,9 @@ export default function Home() {
       </Head>
 
       <main>
-      <TestFunction></TestFunction>
+        <TestFunction></TestFunction>
+        <Circle />
+
         <h1>LightSpeed</h1>
       </main>
     </div>
